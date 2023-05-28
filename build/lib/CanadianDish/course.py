a2a034ec2091cf5000928ba8b26bd6f780d1cee0
heliome <89141577+heliome@users.noreply.github.com>
2023-05-28 11:00:03 +0200

    Add files via upload

--- build/lib/CanadianDish/course.py
+++ build/lib/CanadianDish/course.py
@@ -1,18 +1,26 @@
 import recipe
+import pdf
+import adds
 
 def getRecipe():
-    course = input()
-    if course.lower() == "dinner" or course.lower() == "lunch":
-        n = "?f%5B0%5D=meal%3A46" 
-    elif course.lower() == "breakfast":
-        n = "?f%5B0%5D=meal%3A45"
-    elif course.lower() == "snack":
-        n = "?f%5B0%5D=meal%3A47"
-    else:
-        return("check the name of the course")
+    try:
+        #Check the course and give needed url string
+        course = input()
+        if course.lower() == "dinner" or course.lower() == "lunch":
+            z = "f%5B0%5D=meal%3A46&" 
+        elif course.lower() == "breakfast":
+            z = "f%5B0%5D=meal%3A45&"
+        elif course.lower() == "snack":
+            z = "f%5B0%5D=meal%3A47&"
+        else:
+            return print("Error: check the name of the course")
 
-
-    text = recipe.rand(n)
-    print(text)
+        #Get all the data and create pfd of it
+        img, name, preparation, cooking, serves, ingredients, steps, tips = recipe.rand(z)
+        riddles, drinks = adds.add()
+        pdf.create_pdf(img, name, preparation, cooking, serves, ingredients, steps, tips, riddles, drinks)
+    
+    except:
+        print("Sorry, we have a problem, please try again later")
 
 getRecipe()
--- build/lib/CanadianDish/recipe.py
+++ build/lib/CanadianDish/recipe.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import random
-import pdf
-from reportlab.pdfbase.pdfmetrics import stringWidth
 
 def rand(n=""):
+    #Function to start
+    #Gets the random page from thesite with food
+
     url = f"https://food-guide.canada.ca/en/recipes/?{n}page=0"
 
     r = requests.get(url)
@@ -15,6 +16,7 @@ def rand(n=""):
 
 
 def meal(k, n=""):
+    #Gets random dish from the page
     url = f"https://food-guide.canada.ca/en/recipes/?{n}page={k}"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, features="html.parser")
@@ -24,49 +26,52 @@ def meal(k, n=""):
     return about(site)
 
 def about(site):
+    #Gets all the data about recipe
     url = f"https://food-guide.canada.ca{site}"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, features="html.parser")
+    #Get name
     main = soup.find("h1",{"class":"page_text"})
     name = main.text
 
+    #Get preparation data
     prep = []
     main = soup.find("div",{"class":"recipe-prep-information row"}).find_all("div",{"class":"item"})
     for i in main:
         i = i.find_all("div")
         prep.append(i[0].text.split("\n")[2])
 
+    #Get ingredients
     ingredients = dict()
     main = soup.find("section",{"class":"block block-ctools-block block-entity-fieldnodefield-ingredients clearfix"})
     ingr = main.find_all("ul")
     bonus = main.find_all('p')
-    print(name)
-    print(bonus)
+
     for i in range(len(bonus)-1):
         if bonus[i].text == "" or bonus[i].text == " ":
             bonus.pop(i)
     if len(ingr) == len(bonus):
         s=0
         for i in bonus:
-            k = ingr[s].text.replace(u'\xa0', u' ').replace("⅓", "1/3").split("\n")
+            k = ingr[s].text.replace(u'\xa0', u' ').replace("⅓", "1/3").replace("⅔", "2/3").split("\n")
             k.pop(-1)
             ingredients[i] = k
             s +=1
     else:
-        k = ingr[0].text.replace(u'\xa0', u' ').replace("⅓", "1/3").split("\n")
+        k = ingr[0].text.replace(u'\xa0', u' ').replace("⅓", "1/3").replace("⅔", "2/3").split("\n")
         k.pop(-1)
         ingredients["Ingredients"] = k
 
         if bonus != None:
             s=1
             for i in bonus:
-                k = ingr[s].text.replace(u'\xa0', u' ').replace("⅓", "1/3").split("\n")
+                k = ingr[s].text.replace(u'\xa0', u' ').replace("⅓", "1/3").replace("⅔", "2/3").split("\n")
                 k.pop(-1)
                 ingredients[i] = k
                 s +=1
     
     
-
+    #Get all the steps
     steps = []
     main = soup.find("section",{"class":"block block-ctools-block block-entity-fieldnodefield-directions clearfix"})
     main = main.text.split("\n")
@@ -76,6 +81,7 @@ def about(site):
         steps.pop(0)
         steps.pop(-1)
 
+    #Get all tips
     tips=[]
     main = soup.find("div",{"class":"field field--name-field-cooking-tips field--type-text-long field--label-above"})
     main = main.text.split("\n")
@@ -86,6 +92,7 @@ def about(site):
         tips.pop(0)
         tips.pop(-1)
 
+    #Get tips
     img = soup.find("div",{"class":"featured-image-wrapper"}).find("img")["src"]
     data = requests.get(img).content
     f = open("img.jpg",'wb')
@@ -93,6 +100,8 @@ def about(site):
     f.close()
     
     
-    return pdf.create_pdf("img.jpg",name, prep[0], prep[1], prep[2], ingredients, steps, tips)
 
-rand()
+    return "img.jpg", name, prep[0], prep[1], prep[2], ingredients, steps, tips
+
+
+
